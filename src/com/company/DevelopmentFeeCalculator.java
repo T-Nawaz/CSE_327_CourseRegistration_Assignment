27660033e0f1ad1799942a1be2dc9f316486bc5d
Abid Hasan <abid.northsouth@gmail.com>
2017-03-21 20:03:45 +0600

    Revision 1
    Assignment Part 1

--- src/com/company/DevelopmentFeeCalculator.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class DevelopmentFeeCalculator implements IExtraFreeCalculator {
-    public int getExtraAmount(int courseTotal) {
-        return (int) Math.ceil(courseTotal * 0.1);
-    }
-}
--- src/com/company/IDiscountStrategy.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-
-/**
- * Created by Abid Hasan on 2/28/2017.
- */
-public interface IDiscountStrategy {
-    public int getTotal(Registration registration);
-}
